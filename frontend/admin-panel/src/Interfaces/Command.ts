afff76b8bb2e4cd2d34accc5ac46596c39433e38
Mikycid <lepavecmike@gmail.com>
2024-11-21 00:29:02 +0100

    #25 feat: added argument parsing to console

--- frontend/admin-panel/src/Interfaces/Command.ts
+++ frontend/admin-panel/src/Interfaces/Command.ts
@@ -1,6 +1,6 @@
 import { User } from "./User";
 
 export interface Command {
-    execute: (user: User, args: any[], addOutput: (message: string) => void) => void;
+    execute: (user: User, args: any, addOutput: (message: string) => void) => void;
     help: string;
 }
